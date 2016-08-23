1af2e197692bf3a9c1b1b6c98bfc1398c518fce6
swax97 <sedna9801@gmail.com>
2016-08-23 11:19:03 -0400

    Installed babel and inferno

--- index.js
+++
@@ -1,7 +0,0 @@
-var server = require("serve-me")({
-	debug: true,
-	log: true,
-	directory: "./www"
-});	
-
-server.start(8080);
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import Inferno from 'inferno';
+import InfernoServer from 'inferno-server';
+
+InfernoServer.renderToString(<div>Hello World</div>);
